82cbd07870898456b7b0b57334724c9be9e00b7e
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-09-18 21:59:55 -0500

    Test: example

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .tasks import send_mail
 
 # Create your views here.
 def index(request):
+    mail_sent = False
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        send_mail(email)
+        mail_sent = True
+    
     return render(request, 'index.html',{
-        'mail_sent': False
+        'mail_sent': mail_sent
     })
